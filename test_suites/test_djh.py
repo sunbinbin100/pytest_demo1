e00aa903b3f797d8818a8ca5134e3cc518b63c7f
sunbinbin0001 <191415770@qq.com>
2021-01-14 14:04:49 +0800

    fix mark.run

--- test_suites/test_djh.py
+++ test_suites/test_djh.py
@@ -17,7 +17,7 @@ class TestDJHLogin(object):
         """
         self.DJHLogin = DJHLogin()
 
-    @pytest.mark.run12
+    @pytest.mark.run
     # @pytest.mark.parametrize("x", [1, 2, 3])           # 参数组合
     @pytest.mark.parametrize("userinfo", user_info)      # 参数化
     def test_djh_login(self, userinfo):
@@ -33,7 +33,7 @@ class TestDJHLogin(object):
         allure.attach('<body>allure.attach测试文本text111111</body>', name='html测试块，名称可省略', attachment_type=allure.attachment_type.HTML)
         allure.attach.file('/Users/sunbinbin/Desktop/测试用文件/日结.png', name='日结图片', attachment_type=allure.attachment_type.JPG)           # .file
 
-    @pytest.mark.run12                                        # skip跳过执行
+    @pytest.mark.run                                        # skip跳过执行
     def test_djh_personal_center(self):
         """
         测试电竞虎个人中心接口(get)
